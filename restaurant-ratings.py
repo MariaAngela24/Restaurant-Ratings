c6ef9b8fd6c59f87a120f44f905a6138b1e277a2
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:35:24 -0700

    Restaurant Ratings Lab

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -4,10 +4,17 @@ import sys
 alphabetical_ratings = {}
 
 filename = sys.argv[1]
-for lines in open(filename):
-    lines_clean = lines.rstrip()
-    words = lines_clean.split(':')
-    alphabetical_ratings[words[0]] = words[1]
+for line in open(filename):
+    clean_line = line.rstrip()
+
+    #words = clean_line.split(':')
+    #alphabetical_ratings[words[0]] = words[1]
+
+    restaurant, rating = clean_line.split(':')
+    alphabetical_ratings[restaurant] = rating
+
+    #dictionary[key] = value
 
 for restaurant, rating in sorted(alphabetical_ratings.items()):
+                          # [(k,v), (k,v)]
     print "{} is rated at {}.".format(restaurant, rating)
